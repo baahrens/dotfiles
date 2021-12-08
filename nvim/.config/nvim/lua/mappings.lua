c0604ed3168970a0275c7488568b1d03734adbd1
Bastian Ahrens <bahrens@compeon.de>
2021-12-08 14:18:34 +0100

    Remove unused package-info mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -150,11 +150,3 @@ u.remap('n', 'ga',         vim.lsp.diagnostic.get_line_diagnostics, { silent = t
 u.remap('i', '<C-x><C-x>', vim.lsp.buf.signature_help,              { silent = true })
 u.remap('n', 'gm',         lsp.format_range_operator,               { silent = true })
 u.remap('n', 'gf',         vim.lsp.buf.formatting,                  { silent = true })
-
-u.remap("n", "<leader>ns", ":lua require('package-info').show()<CR>",           { silent = true, noremap = true })
-u.remap("n", "<leader>nc", ":lua require('package-info').hide()<CR>",           { silent = true, noremap = true })
-u.remap("n", "<leader>nu", ":lua require('package-info').update()<CR>",         { silent = true, noremap = true })
-u.remap("n", "<leader>nd", ":lua require('package-info').delete()<CR>",         { silent = true, noremap = true })
-u.remap("n", "<leader>ni", ":lua require('package-info').install()<CR>",        { silent = true, noremap = true })
-u.remap("n", "<leader>nr", ":lua require('package-info').reinstall()<CR>",      { silent = true, noremap = true })
-u.remap("n", "<leader>np", ":lua require('package-info').change_version()<CR>", { silent = true, noremap = true })
