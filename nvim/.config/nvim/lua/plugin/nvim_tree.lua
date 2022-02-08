48a595e0d93c5b09183a5d45242aa39ff9463104
Bastian Ahrens <bahrens@compeon.de>
2022-02-08 11:57:16 +0100

    Adjust nvim-tree settings

--- nvim/.config/nvim/lua/plugin/nvim_tree.lua
+++ nvim/.config/nvim/lua/plugin/nvim_tree.lua
@@ -1,21 +1,6 @@
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 local nvim_tree = require'nvim-tree'
 
-vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.DS_Store' }
-vim.g.nvim_tree_quit_on_open = 1
-vim.g.nvim_tree_special_files = {
-  ['package.json'] = 1,
-  ['index.js'] = 1,
-  ['init.lua'] = 1
-}
-
-vim.g.nvim_tree_show_icons = {
-  ['git'] = 1,
-  ['folders'] = 1,
-  ['files'] = 0,
-  ['folder_arrows'] = 1
-}
 
 vim.g.nvim_tree_icons = {
   symlink = "",
@@ -42,9 +27,12 @@ vim.g.nvim_tree_icons = {
 
 nvim_tree.setup {
   auto_close = true,
-  follow = true,
-  git_hl = true,
   update_cwd = true,
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 500,
+  },
   diagnostics = {
     enable = true,
     icons = {
@@ -63,8 +51,9 @@ nvim_tree.setup {
     side = "left",
     mappings = {
       list = {
-        { key = "v", cb = tree_cb("vsplit") },
-        { key = "s", cb = tree_cb("split") }
+        { key = "v", action = "vsplit" },
+        { key = "s", action = "split" },
+        { key = "x", action = "close_node" }
       }
     }
   }
