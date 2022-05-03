c43161693acc5ee1cef17a5ecbefe476f34fed5d
Bastian Ahrens <bahrens@compeon.de>
2022-05-03 13:52:36 +0200

    Use english language for fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,9 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="$HOME/.rbenv/shims:$PATH"
 
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
 abbr -a g git
 abbr -a v nvim
 
