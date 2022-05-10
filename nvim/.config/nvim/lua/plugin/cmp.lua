c08d39968cf6d66d512d88aef669c4441bdea19a
Bastian Ahrens <bahrens@compeon.de>
2022-05-10 20:58:02 +0200

    Neovim: Fix cmp cmdline mappings

--- nvim/.config/nvim/lua/plugin/cmp.lua
+++ nvim/.config/nvim/lua/plugin/cmp.lua
@@ -28,6 +28,7 @@ local kind_icons = {
   Operator = "",
   TypeParameter = ""
 }
+
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -48,7 +49,7 @@ cmp.setup({
       luasnip.lsp_expand(args.body)
     end,
   },
-  mapping = {
+  mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
@@ -77,7 +78,7 @@ cmp.setup({
         fallback()
       end
     end, { "i", "s" }),
-  },
+  }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp',                max_item_count = 10 },
     { name = "nvim_lsp_signature_help", max_item_count = 10 },
@@ -95,12 +96,14 @@ cmp.setup({
 })
 
 require'cmp'.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'cmdline' }
   }
 })
 
 require'cmp'.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' }
   }
