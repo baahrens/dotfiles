633dc75fe490ebc1afcf4aa3611d7952ff36b898
Bastian Ahrens <bahrens@compeon.de>
2022-07-11 23:08:24 +0200

    Neovim: Add rust_analyzer to format clients

--- nvim/.config/nvim/lua/plugin/lsp/init.lua
+++ nvim/.config/nvim/lua/plugin/lsp/init.lua
@@ -32,23 +32,16 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
-function M.format_range_operator()
-  local old_func = vim.go.operatorfunc
-  _G.op_func_formatting = function()
-    local start = vim.api.nvim_buf_get_mark(0, '[')
-    local finish = vim.api.nvim_buf_get_mark(0, ']')
-    vim.lsp.buf.range_formatting({}, start, finish)
-    vim.go.operatorfunc = old_func
-    _G.op_func_formatting = nil
-  end
-  vim.go.operatorfunc = 'v:lua.op_func_formatting'
-  vim.api.nvim_feedkeys('g@', 'n', false)
-end
+local format_clients = {
+    "null-ls",
+    "prismals",
+    "rust_analyzer"
+}
 
 function M.format()
   vim.lsp.buf.format {
     filter = function(lsp_client)
-      return lsp_client.name == "null-ls" or lsp_client.name == "prismals"
+      return vim.tbl_contains(format_clients, lsp_client.name)
     end
   }
 end
