a9424ffa735d68319ab038d66d4a73d5fa509c7e
Bastian Ahrens <bahrens@compeon.de>
2022-07-26 18:40:24 +0200

    Neovim: Enable js snippets for ts

--- nvim/.config/nvim/lua/plugin/luasnip.lua
+++ nvim/.config/nvim/lua/plugin/luasnip.lua
@@ -1,1 +1,5 @@
 require("luasnip/loaders/from_vscode").lazy_load()
+ require('luasnip').filetype_extend("typescript", { "javascript" })
+ require('luasnip').filetype_extend("typescriptreact", { "javascript" })
+
+
