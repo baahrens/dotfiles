49fa300b9859ac446551e419c4ca278630a019c4
Bastian Ahrens <bahrens@compeon.de>
2022-09-15 21:49:23 +0200

    Neovim: Improve cybu

--- nvim/.config/nvim/lua/plugin/cybu.lua
+++ nvim/.config/nvim/lua/plugin/cybu.lua
@@ -1,1 +1,25 @@
-require("cybu").setup()
+require("cybu").setup({
+  position = {
+    relative_to = "win",
+    anchor = "topright"
+  },
+  behavior = {
+    mode = {
+      default = {
+        switch = "immediate",
+        view = "paging"
+      },
+    }
+  },
+  style = {
+    padding = 4,
+    hide_buffer_id = true,
+    border = "rounded"
+  },
+  display_time = 700,
+  exclude = {
+    "nvim-tree",
+    "fugitive",
+    "trouble"
+  },
+})
