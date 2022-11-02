b831bd269732f1f5d3d044a1ac04dc5a24e4adba
Bastian Ahrens <bahrens@compeon.de>
2022-11-02 18:36:07 +0100

    Neovim: Improve diagnostics float styles

--- nvim/.config/nvim/lua/plugin/lsp/init.lua
+++ nvim/.config/nvim/lua/plugin/lsp/init.lua
@@ -1,3 +1,21 @@
+local BORDER = {
+  "╭",
+  "─",
+  "╮",
+  "│",
+  "╯",
+  "─",
+  "╰",
+  "│",
+}
+
+local SIGNS = {
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Info = " "
+}
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
   vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
@@ -11,30 +29,16 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
     severity_sort = true,
   }
 )
-local border = {
-  "╭",
-  "─",
-  "╮",
-  "│",
-  "╯",
-  "─",
-  "╰",
-  "│",
-}
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = BORDER })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = BORDER })
+vim.lsp.handlers['textDocument/references'] = function()
+  require('telescope.builtin').lsp_references()
+end
 
 local M = {}
 
-local signs = {
-  Error = " ",
-  Warn = " ",
-  Hint = " ",
-  Info = " "
-}
-
-for type, icon in pairs(signs) do
+for type, icon in pairs(SIGNS) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
@@ -67,5 +71,12 @@ function M.on_attach (client)
 end
 
 M.capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+vim.diagnostic.config({
+  virtual_text = { spacing = 4, prefix = '●' },
+  float = { border = BORDER, source = 'if_many' },
+  signs = true,
+  update_in_insert = false,
+})
 
 return M
