4d0029bbbdce5239f12de33925ea13c29a0c7c56
Bastian Ahrens <ahrens.bastian@afb.de>
2023-03-02 22:07:31 +0100

    Neovim: Fix lua lsp

--- nvim/.config/nvim/lua/plugin/lsp/lua.lua
+++ nvim/.config/nvim/lua/plugin/lsp/lua.lua
@@ -1,45 +1,12 @@
 local lsp = require('plugin/lsp')
 
-local system_name
-if vim.fn.has("mac") == 1 then
-  system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-  system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
-  system_name = "Windows"
-else
-  print("Unsupported system for sumneko")
-end
-
-local sumneko_root_path = '/Users/b/dev/projects/lua-language-server'
-local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
-
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-require'lspconfig'.sumneko_lua.setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-  capabilities = lsp.capabilities,
-  on_attach = lsp.on_attach,
+require'lspconfig'.lua_ls.setup({
+  debounce_text_changes = 500,
   settings = {
     Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        path = runtime_path,
-      },
-      diagnostics = {
-        globals = { 'vim', 'use' },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-  flags = {
-    debounce_text_changes = 500
+      completion = {
+        callSnippet = "Replace"
+      }
+    }
   }
-}
+})
