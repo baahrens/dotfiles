eb9ec4b3e358fcc596f75c6a1e77a7fc36a2e83d
baahrens <ba92@protonmail.com>
2023-03-15 21:57:39 +0100

    Neovim: Adjust dressing config

--- nvim/.config/nvim/lua/plugin/dressing.lua
+++ nvim/.config/nvim/lua/plugin/dressing.lua
@@ -1,7 +1,35 @@
 require('dressing').setup({
   input = {
+    enable = true,
     win_options = {
       winblend = 0,
-    }
-  }
+    },
+    prompt_align = "left",
+    border = "rounded",
+    anchor = "NW",
+    mappings = {
+      n = {
+        ['<Esc>'] = 'Close',
+        ['<CR>'] = 'Confirm',
+      },
+      i = {
+        ['<C-c>'] = 'Close',
+        ['<CR>'] = 'Confirm',
+        ['<Up>'] = 'HistoryPrev',
+        ['<Down>'] = 'HistoryNext',
+      },
+    },
+  },
+  select = {
+    enable = true,
+    win_options = {
+      winblend = 0,
+    },
+    border = "rounded",
+    mappings = {
+      ['<Esc>'] = 'Close',
+      ['<C-c>'] = 'Close',
+      ['<CR>'] = 'Confirm',
+    },
+  },
 })
