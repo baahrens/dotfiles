8789b8b76d110b27277ad7bcbab42d1b1c2a758e
Bastian Ahrens <bahrens@compeon.de>
2023-03-17 10:06:41 +0100

    Neovim: Disable indent lines for some filetypes

--- nvim/.config/nvim/lua/plugin/indent.lua
+++ nvim/.config/nvim/lua/plugin/indent.lua
@@ -1,4 +1,5 @@
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
+    filetype_exclude = { "help", "alpha", "nvim-tree", "Trouble", "lazy" },
 }
