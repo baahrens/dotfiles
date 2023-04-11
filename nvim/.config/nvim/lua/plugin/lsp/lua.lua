3a4c938011276c9fcde26ab217a2df1dd3d39df7
Bastian Ahrens <bahrens@compeon.de>
2023-04-11 16:44:44 +0200

    Neovim: Configure lualsp for nvim development

--- nvim/.config/nvim/lua/plugin/lsp/lua.lua
+++ nvim/.config/nvim/lua/plugin/lsp/lua.lua
@@ -1,11 +1,27 @@
-local lsp = require('plugin/lsp')
+local lsp = require("plugin/lsp")
 
-require'lspconfig'.lua_ls.setup({
-  capabilities = lsp.capabilities,
-  on_attach = function(client)
-    client.server_capabilities.document_formatting = false
-    client.server_capabilities.document_range_formatting = false
+require("lspconfig").lua_ls.setup({
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+	capabilities = lsp.capabilities,
+	on_attach = function(client)
+		client.server_capabilities.document_formatting = false
+		client.server_capabilities.document_range_formatting = false
 
-    lsp.on_attach(client)
-  end
+		lsp.on_attach(client)
+	end,
 })
