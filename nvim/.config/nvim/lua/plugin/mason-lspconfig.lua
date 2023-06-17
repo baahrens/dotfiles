30044f383ef982c6fd34f93e4d67215af1d5cbc2
Bastian Ahrens <bahrens@compeon.de>
2023-06-18 00:34:05 +0200

    Neovim: Ensure installed lsp

--- nvim/.config/nvim/lua/plugin/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugin/mason-lspconfig.lua
@@ -1,10 +1,13 @@
 require("mason-lspconfig").setup({
   automatic_installation = true,
   ensure_installed = {
+    "prismals",
     "lua_ls",
     "rust_analyzer",
     "tsserver",
     "jsonls",
     "cssls",
+    "tailwindcss",
+    "vtsls"
   },
 })
