5616e281dcaf732f1d7b7acc08a9a5df44094090
Bastian Ahrens <bastianahrens92@gmail.com>
2023-09-03 15:57:43 +0200

    Neovim: Revert cmp mappings

--- nvim/.config/nvim/lua/plugin/cmp.lua
+++ nvim/.config/nvim/lua/plugin/cmp.lua
@@ -68,8 +68,8 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<C-u>"] = cmp.mapping.scroll_docs(-4),
 		["<C-d>"] = cmp.mapping.scroll_docs(4),
-		["<leader><leader>h"] = cmp.mapping.close(),
-		["<leader><leader>l"] = cmp.mapping.confirm({
+		["<C-s>"] = cmp.mapping.close(),
+		["<C-a>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
@@ -87,24 +87,6 @@ cmp.setup({
 			end
 		end, { "i", "s" }),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<leader><leader>j"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<leader><leader>k"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
