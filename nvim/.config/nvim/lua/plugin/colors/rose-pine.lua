1ea8a9345583ac3cc50e883331cd14a050aaf984
Bastian Ahrens <bastianahrens92@gmail.com>
2023-10-04 09:46:34 +0200

    Neovim: Adjust rose-pine colors for Telescope

--- nvim/.config/nvim/lua/plugin/colors/rose-pine.lua
+++ nvim/.config/nvim/lua/plugin/colors/rose-pine.lua
@@ -4,7 +4,13 @@ function M.setup()
   require('rose-pine').setup({
     variant = "moon",
     disable_italics = true,
-    disable_background = true
+    disable_background = true,
+    highlight_groups = {
+      TelescopeBorder = { fg = "highlight_high", bg = "none" },
+      TelescopeNormal = { bg = "none" },
+      TelescopePromptNormal = { bg = "base" },
+      TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+    },
   })
 end
 
