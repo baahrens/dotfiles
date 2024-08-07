9bb355754eba96bfeedf1c89bbd7e908783b7606
Bastian Ahrens <bastianahrens92@gmail.com>
2024-08-07 08:58:50 +0200

    Neovim: Remove gitsigns signs

--- nvim/.config/nvim/lua/plugin/gitsigns.lua
+++ nvim/.config/nvim/lua/plugin/gitsigns.lua
@@ -1,10 +1,3 @@
 require("gitsigns").setup({
   numhl = false,
-  signs = {
-    add = { hl = "GitSignsAdd", text = "│" },
-    change = { hl = "GitSignsChange", text = "│" },
-    delete = { hl = "GitSignsDelete", text = "│" },
-    changedelete = { hl = "GitSignsChange", text = "│" },
-    topdelete = { hl = "GitSignsDelete", text = "‾" },
-  },
 })
