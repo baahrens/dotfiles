05e5a4c7cd767417125781379bea17f82a4b8389
Bastian Ahrens <ahrens.bastian@afb.de>
2024-11-19 10:05:47 +0100

    Neovim: Revert removal of null-ls

---
+++ nvim/.config/nvim/lua/plugin/null-ls.lua
@@ -0,0 +1,32 @@
+local null_ls = require("null-ls")
+local settings = require("settings")
+
+local diagnostics = null_ls.builtins.diagnostics
+local actions = null_ls.builtins.code_actions
+
+null_ls.setup({
+  debug = true,
+  sources = {
+    diagnostics.eslint_d,
+
+    actions.eslint_d,
+    actions.gitsigns,
+  },
+  flags = {
+    debounce_text_changes = 200,
+  },
+  on_attach = function(client)
+    client.server_capabilities.document_formatting = true
+    client.server_capabilities.document_range_formatting = true
+
+    require("plugin/lsp").on_attach(client)
+  end,
+})
+
+if settings.format.prettier then
+  null_ls.register(null_ls.builtins.formatting.prettierd)
+end
+
+if settings.format.eslint then
+  null_ls.register(null_ls.builtins.formatting.eslint_d)
+end
