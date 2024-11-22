0e26be484e0677c48a7cdeb7fc646936c34c91de
Bastian Ahrens <ahrens.bastian@afb.de>
2024-11-22 23:51:02 +0100

    Neovim: Use rose-pine background

--- nvim/.config/nvim/lua/plugin/colors/rose-pine.lua
+++ nvim/.config/nvim/lua/plugin/colors/rose-pine.lua
@@ -4,7 +4,7 @@ function M.setup()
   require('rose-pine').setup({
     variant = "moon",
     disable_italics = true,
-    disable_background = true,
+    disable_background = false,
     highlight_groups = {
       TelescopeBorder = { fg = "highlight_high", bg = "none" },
       TelescopeNormal = { bg = "none" },
