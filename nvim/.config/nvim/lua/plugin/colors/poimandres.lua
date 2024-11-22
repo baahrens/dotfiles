c29cd73ffa6bfc6f7d2563006b4abec427431a83
Bastian Ahrens <ahrens.bastian@afb.de>
2024-11-23 00:00:41 +0100

    Neovim: Fix poimandres TelescopePreviewTitle color

--- nvim/.config/nvim/lua/plugin/colors/poimandres.lua
+++ nvim/.config/nvim/lua/plugin/colors/poimandres.lua
@@ -1,9 +1,13 @@
 local M = {}
 
 function M.setup()
-  require('poimandres').setup {
-    dim_nc_background = false
-  }
+  require("poimandres").setup({
+    dim_nc_background = false,
+  })
 end
+
+M.highlight_overwrites = {
+  TelescopePreviewTitle = { fg = "none" },
+}
 
 return M
